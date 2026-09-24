6958bb14640e849267609a0ed4146b69023fcd09
Dhaksha005 <studioadyas@gmail.com>
2026-09-24 10:35:38 +0530

    Add bstpost.py

---
+++ bstpost.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.key:
+        root.left = insert(root.left, key)
+    elif key > root.key:
+        root.right = insert(root.right, key)
+    return root
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.key, end=" ")
+        inorder(root.right)
+
+def postorder(root):
+    if root:
+       
+        postorder(root.left)
+       
+        postorder(root.right)
+        
+        print(root.key, end=" ")
+
+root = None
+values = [50, 30, 70, 20, 40, 60, 80]
+for value in values:
+    root = insert(root, value)
+
+print("Inorder Traversal:")
+inorder(root)
+print("\n")
+
+print("Postorder Traversal:")
+postorder(root)
+print()
